25b2a5dc0522a639956c650999002b47238adc66
M-Amer <mohamedamerjalal@gmail.com>
2024-10-07 21:47:50 +0300

    update ViewModel Woof

--- app/src/main/java/com/example/woof/MyViewModel.kt
+++ app/src/main/java/com/example/woof/MyViewModel.kt
@@ -1,8 +1,8 @@
 package com.example.woof
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.compose.runtime.State
 
 class MyViewModel : ViewModel() {
     // State to track if expanded or not
